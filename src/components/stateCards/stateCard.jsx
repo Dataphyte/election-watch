23e2874dfa509aad9bad312f3a2d19d9cc9f4e27
Oluwarotimi Adewale <merlinwaysgaming@gmail.com>
2023-02-26 16:58:31 +0100

    working on state card

--- src/components/stateCards/stateCard.jsx
+++ src/components/stateCards/stateCard.jsx
@@ -8,7 +8,14 @@ const StateCard = () => {
     <div className='text-black w-full min-h-full flex  items-center gap-5 justify-evenly '>
       {states &&
         states.map((data, idx) => (
-          <div className='w-[50%] h-[250px] border-red-500 border rounded-md ' key={idx}></div>
+          <div className='w-[50%] h-[250px] border-red-500 border rounded-md p-3' key={idx}>
+            <h1 className='font-bold text-blue-900 text-lg'>{data.name}</h1>
+            <section className='flex justify-between items-start gap-[40px] mt-2'>
+                <h1 className='flex-0'>Party</h1>
+                <h1 className='flex-0'>Percentage</h1>
+                <h1 className='flex-1 border-black'>Votes</h1>
+            </section>
+          </div>
         ))}
     </div>
   );
