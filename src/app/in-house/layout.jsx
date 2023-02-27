632d310549d37c8a921e5667005084779274bfa9
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2023-02-27 14:56:14 +0100

    dasfwdc

--- src/app/in-house/layout.jsx
+++ src/app/in-house/layout.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/navigation';
 import { superUserStore } from '@/global/superUserStore';
 
@@ -8,16 +8,16 @@ const InHouseLayout = ({ children }) => {
   const { superUser } = superUserStore();
   const navigator = useRouter();
 
-  if (!superUser || superUser === null || !superUser.user) {
-    navigator.push('in-house');
-  }
+  //   if (!superUser || superUser === null || !superUser.user) {
+  //     navigator.push('in-house');
+  //   }
 
-  // -- managhe user access -->
-  //   useEffect(() => {
-  //     if (!superUser || superUser === null || !superUser.user) {
-  //       navigator.push('in-house');
-  //     }
-  //   });
+  //   -- managhe user access -->
+  useEffect(() => {
+    if (!superUser || superUser === null || !superUser.user) {
+      navigator.push('in-house');
+    }
+  });
 
   return <div>{children}</div>;
 };
